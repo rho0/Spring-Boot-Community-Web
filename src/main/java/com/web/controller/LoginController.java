b64d43ff3c21777cfbbd244b46a99a1ee0ad3ecf
rho0 <lgnay123@gmail.com>
2021-12-08 16:45:03 +0900

    Spring-Boot-Community-Web 예제5-17까지

---
+++ src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.web.controller;
+
+
+import com.web.domain.User;
+import com.web.domain.enums.SocialType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value = "/{facebook|google|kakao}/complete") //인증이 성공적으로 처리된 이후에 리다이렉트되는 경로.
+    public String loginComplete(HttpSession session){
+        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+
+        Map<String, String> map = (HashMap<String, String>)authentication.getUserAuthentication().getDetails();
+        session.setAttribute("user",  User.builder()
+                                                .name(map.get("name"))
+                                                .email(map.get("email"))
+                                                .principal(map.get("id"))
+                                                .socialType(SocialType.FACEBOOK).createdDate(LocalDateTime.now()).build());
+
+        return "redirect:/board/list";
+
+    }
+
+
+
+
+}
--- src/main/java/com/web/oauth/ClientResources.java
+++ src/main/java/com/web/oauth/ClientResources.java
@@ -1,7 +1,7 @@
 package com.web.oauth;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
 public class ClientResources {
@@ -10,14 +10,14 @@ public class ClientResources {
     private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
 
     @NestedConfigurationProperty
-    public OAuth2ResourceServerProperties resource = new OAuth2ResourceServerProperties();
+    public ResourceServerProperties resource = new ResourceServerProperties();
 
 
     public AuthorizationCodeResourceDetails getClient() {
         return client;
     }
 
-    public OAuth2ResourceServerProperties getResource() {
+    public ResourceServerProperties getResource() {
         return resource;
     }
 
